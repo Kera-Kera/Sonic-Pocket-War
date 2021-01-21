ac64f8de11b27e6829dbe495ad81bdfb5275aa11
Kera-Kera <56071574+Kera-Kera@users.noreply.github.com>
2021-01-21 12:07:31 +0000

    Tidy Code

--- Sonic Pocket War/Assets/Scripts/PlayerScript.cs
+++ Sonic Pocket War/Assets/Scripts/PlayerScript.cs
@@ -5,23 +5,15 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    public float speed = 0f;
+    private float speed = 0f;
+    [SerializeField]
+    private float maxSpeed = 5f, acceleration = 1f, jumpHeight = 5f;
 
-    public float maxSpeed = 5f;
+    [SerializeField]
+    private LayerMask platformLayerMask;
 
-    public float minSpeed = -5f;
-
-    public float acceleration = 1f; 
-
-    public LayerMask platformLayerMask;
-
-    public float jumpHeight = 5f;
-
-    [SerializeField]private bool isGrounded = false;
-
-    Rigidbody2D rigidbody2d;
-
-    BoxCollider2D boxcollider2d;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxcollider2d;
 
     void Start()
     {
@@ -32,42 +24,41 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         GroundCheck();
+        InputController();
+        PlayerSpeed();
 
-
-        //speed -= acceleration * Time.deltaTime;
-
-        if (speed > maxSpeed)
-            speed = maxSpeed;
-
-        if (speed < minSpeed)
-            speed = minSpeed;
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            speed -= acceleration * Time.deltaTime;
-            
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            speed += acceleration * Time.deltaTime;
-
-        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && GroundCheck())
-        {
-            speed /= 1.003f;
-        }
-
-            rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space) && GroundCheck())
-        {
-            Jump();
-        }
     }
 
     private void Jump()
     {
         rigidbody2d.velocity.Set(rigidbody2d.velocity.x,0);
         rigidbody2d.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
+    }
 
-        isGrounded = false;
+    private void PlayerSpeed()
+    {
+        if (speed > maxSpeed)
+            speed = maxSpeed;
+        if (speed < -maxSpeed)
+            speed = -maxSpeed;
+
+        rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+    }
+
+    private void InputController()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            speed -= acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+            speed += acceleration * Time.deltaTime;
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && GroundCheck())
+        {
+            speed /= 1.003f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && GroundCheck())
+        {
+            Jump();
+        }
     }
 
     private bool GroundCheck()
