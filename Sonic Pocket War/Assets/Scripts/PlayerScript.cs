9c85cc5b4b13d014f9c7192451a28506763ca35a
ThatSkullKid <ThatSkullKid@users.noreply.github.com>
2021-02-01 17:32:54 +0000

    Initial state implementation
    Adding in a state dictionary as well as basic states for both moving and idle

--- Sonic Pocket War/Assets/Scripts/PlayerScript.cs
+++ Sonic Pocket War/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
+    private Dictionary<string,bool> stateArray = new Dictionary<string, bool>();
+
+    [SerializeField]
     private float speed = 0f;
     [SerializeField]
     private float maxSpeed = 5f, acceleration = 1f, jumpHeight = 5f;
@@ -12,28 +15,52 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     private LayerMask platformLayerMask;
 
+
+
+
+
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxcollider2d;
+    private SpriteRenderer objSpriteRenderer;
+
+
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         boxcollider2d = GetComponent<BoxCollider2D>();
+
+        //sets adds all the states to the object dictionary
+
+        stateArray.Add("isIdle", false);
+        stateArray.Add("isMoving", false);
+        stateArray.Add("isJumping", false);
+        stateArray.Add("isFalling", false);
+        stateArray.Add("isAttacking", false);
+
+
+        objSpriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
     {
         GroundCheck();
         InputController();
-        PlayerSpeed();
+        PlayerSpeed();;
 
     }
+
+
+    // Applies a force to the rigidbody to move the object
 
     private void Jump()
     {
         rigidbody2d.velocity.Set(rigidbody2d.velocity.x,0);
         rigidbody2d.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
     }
+
+
 
     private void PlayerSpeed()
     {
@@ -47,18 +74,50 @@ public class PlayerScript : MonoBehaviour
 
     private void InputController()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-            speed -= acceleration * Time.deltaTime;
-        if (Input.GetKey(KeyCode.RightArrow))
-            speed += acceleration * Time.deltaTime;
+
+        // Inputs for right and left actions - Leads to directional movement respectively
+        if (Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.RightArrow))
+        {
+            setStateFalse();
+            stateArray["isMoving"] = true;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                objSpriteRenderer.flipX = true;
+
+                speed -= acceleration * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                objSpriteRenderer.flipX = false;
+                speed += acceleration * Time.deltaTime;
+            }
+        }
+
+
+        // 
+
         if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && GroundCheck())
         {
-            speed /= 1.003f;
+
+            if (speed > -0.03 && speed < 0.03)
+            {
+                speed = 0;
+                StartCoroutine(idleTimer());
+            }
+            else
+            { 
+                speed /= 1.003f;
+            }
         }
+
+
+        // Input for Jump action, checks whether on ground before running function
+
         if (Input.GetKeyDown(KeyCode.Space) && GroundCheck())
         {
             Jump();
         }
+
     }
 
     private bool GroundCheck()
@@ -68,4 +127,29 @@ public class PlayerScript : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(boxcollider2d.bounds.center, boxcollider2d.bounds.size, 0f, Vector2.down, lengthdown, platformLayerMask);
         return hit.collider;
     }
+
+    //Used to set the state to idle after 5 seconds
+
+    IEnumerator idleTimer()
+    {
+        yield return new WaitForSeconds(5);
+        setStateFalse();
+        stateArray["isIdle"] = true;
+    }
+
+    // Sets all states of the player to false, to be run before setting any state to clear all other states
+
+    private void setStateFalse()
+    {
+
+        stateArray["isIdle"] = false;
+        stateArray["isMoving"] = false;
+        stateArray["isJumping"] = false;
+        stateArray["isFalling"] = false;
+        stateArray["isAttacking"] = false;
+
+
+    }
+
+
 }
